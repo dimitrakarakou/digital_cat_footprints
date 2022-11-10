178430d3bfc5450da272950e881e15cc871658e8
Maria Dimitrakarakou <maridimi@gmail.com>
2022-11-10 10:03:36 +0200

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -1,5 +1,4 @@
 <?php
-
 function omega_excerpt_more( $more ) {
 	return ' ... <a id="more-link'. strval(get_the_ID()) . ' href="'. get_permalink( get_the_ID() ) . '">' . omega_get_setting( 'content_archive_more' ) . 
 	'<img class=".catFootprints' . strval(get_the_ID()) . '" src=""></a>';
